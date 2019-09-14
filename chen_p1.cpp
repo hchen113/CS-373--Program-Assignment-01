2a91d5ec8d8bd7dd9a08b149a2011c325f6ebfd6
hchen113 <hchen113@binghamton.edu>
2019-09-13 23:07:13 -0400

    Add trans struct

--- chen_p1.cpp
+++ chen_p1.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
 using namespace std;
 
-int main(){
-  cout << "Hello World" << "\n";
-  return 0;
+struct transitions{
+    int state_1;
+    char symbol_1;
+    int state_2;
+    char symbol_2;
+    char direction;
+};
+
+int main(int argc, char *argv[]){
+
+  string filename = argv[1];
+  string input = argv[2];
+  int trans_num = argv[3];
+
+  int start, accept, reject;
+
+  
+
+
+
+
+  return = 0;
 }
---
+++ makefile
@@ -0,0 +1,4 @@
+make:
+	g++ -o chen_p1 chen_p1.cpp
+clean:
+	rm chen_p1
