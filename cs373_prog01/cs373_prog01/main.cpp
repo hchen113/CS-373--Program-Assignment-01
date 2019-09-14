105035ebd6e062ddac7025a1c49329d7e9b3cf31
Henry Chen <hchen113@binghamton.edu>
2019-09-14 03:46:46 -0400

    Finished the program, just need to debug

--- cs373_prog01/cs373_prog01/main.cpp
+++ cs373_prog01/cs373_prog01/main.cpp
@@ -12,10 +12,87 @@ struct transitions{
     char direction;
 };
 
+struct result{
+    string end_phase;
+    vector<int> state;
+};
+
+result run_turing_machine(vector<transitions> machine_transitions, int start, int accept, int reject, string input_str, int trans_num){
+    result solution;
+    string input;
+    for (int i = 0; i < input_str.length(); i++){
+        if (input_str[i] == '_'){
+            input.push_back(' ');
+            continue;
+        }
+        input.push_back(input_str[i]);
+    }
+    
+    //cout << "FINISH CONVERTING INPUT. \n";
+    //cout << input << "\n";
+    bool done = false;
+    int char_count = 0;
+    int state = start;
+    int transition_count = 0;
+    
+    while (done != true){
+        
+        //cout << "TRANSITION COUNT: " << transition_count << "\n";
+        //cout << "Current State: " << state << " with char: " << input[char_count] << "\n";
+        for (int i = 0; i < machine_transitions.size(); i++){
+            if(machine_transitions[i].state_1 == state && machine_transitions[i].symbol_1 == input[char_count]){
+                if (machine_transitions[i].state_2 == accept){
+                    state = accept;
+                    solution.state.push_back(accept);
+                    solution.end_phase = "accept";
+                    done = true;
+                    //cout << "ACCEPT AT STATE " << state << "\n";
+                    break;
+                }
+                if (machine_transitions[i].state_2 == reject){
+                    state = reject;
+                    solution.state.push_back(reject);
+                    solution.end_phase = "reject";
+                    done = true;
+                    //cout << "REJECT AT STATE " << state << "\n";
+                    break;
+                }
+                if (transition_count > trans_num){
+                    //state = machine_transitions[i].state_2;
+                    //solution.state.push_back(state);
+                    solution.end_phase = "quit";
+                    done = true;
+                    //cout << "QUIT AT STATE " << state << "\n";
+                    break;
+                }
+                solution.state.push_back(state);
+                state = machine_transitions[i].state_2;
+                //cout << "TRANSITIONING TO STATE " << state << " , REPLACING " << input[char_count];
+                input[char_count] = machine_transitions[i].symbol_2;
+                //cout << " WITH " << input[char_count] << "\n";
+                if (machine_transitions[i].direction == 'L'){
+                    //cout << "SHIFTING L \n";
+                    char_count --;
+                    //cout << "CHAR COUNT: " << char_count << "\n";
+                }else{
+                    //cout << "SHIFTING R \n";
+                    char_count ++;
+                    //cout << "CHAR COUNT: " << char_count << "\n";
+                }
+                break;
+            }
+        }
+        transition_count ++;
+    }
+    
+    
+    return solution;
+}
+
 int main(int argc, char *argv[]){
     
     if (argc != 4){
-        cout << "ERROR WITH INPUT \n";
+        cerr << "ERROR WITH INPUT \n";
         exit(1);
     }
     string filename = argv[1];
@@ -26,11 +103,14 @@ int main(int argc, char *argv[]){
     int accept = -1;
     int reject = 1;
     
+    
+    
     ifstream definition_file;
     definition_file.open(filename);
     string line;
     vector<transitions> machine_transations;
     if (definition_file.is_open()){
+        //cout << "OPENING DEFINTION FILE" << "\n";
         while(getline(definition_file, line)){
             //cout << line << "\n";
             int temp_state = -1;
@@ -77,7 +157,11 @@ int main(int argc, char *argv[]){
                                 //cout << "STATE 1: TEMPO " << tempo << "\n";
                                 temp.state_1 = stoi(tempo);
                             }else if (temp.symbol_1 == 'X'){
-                                temp.symbol_1 = line[i];
+                                if (line[i] == '_'){
+                                    temp.symbol_1 = ' ';
+                                }else{
+                                    temp.symbol_1 = line[i];
+                                }
                             }else if (temp.state_2 == -1){
                                 string tempo;
                                 while (line[i] != '\t'){
@@ -88,7 +172,11 @@ int main(int argc, char *argv[]){
 
                                 temp.state_2 = stoi(tempo);
                             }else if (temp.symbol_2 == 'X'){
-                                temp.symbol_2 = line[i];
+                                if (line[i] == '_'){
+                                    temp.symbol_2 = ' ';
+                                }else{
+                                    temp.symbol_2 = line[i];
+                                }
                             }else if (temp.direction == 'X'){
                                 temp.direction = line[i];
                             }
@@ -117,8 +205,21 @@ int main(int argc, char *argv[]){
     for (int i = 0; i < machine_transations.size(); i++){
         cout << machine_transations[i].state_1 << " " << machine_transations[i].symbol_1 << " " << machine_transations[i].state_2 << " " << machine_transations[i].symbol_2 << " " <<
         machine_transations[i].direction << " \n";
+    }*/
+
+    
+    //cout << "DONE WITH ANALZYING INPUT FILE. \n";
+    
+    result temp = run_turing_machine(machine_transations, start, accept, reject, input, trans_num);
+    
+    
+    for (int i = 0; i < temp.state.size(); i++){
+        cout << temp.state[i];
+        if (i < temp.state.size() -1){
+            cout << "->";
+        }
     }
-     */
+    cout << " " << temp.end_phase << "\n";
     
     
     
